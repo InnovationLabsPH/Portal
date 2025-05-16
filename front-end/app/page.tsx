93c9debfb0346461ca0baf035ceecc79c34e1c5b
Mj Advincula <ehm.advincula13@gmail.com>
2025-05-16 14:32:35 +0800

    Add error handling to data fetching in Home component

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -7,9 +7,14 @@ import styles from "./page.module.css";
 export default function Home() {
   useEffect(() => {
     const getData = async () => {
+      try{
       const query = await fetch("https://jsonplaceholder.typicode.com/users");
       const response = await query.json();
       console.log("response hello api", response);
+      }
+      catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
     getData();
   }, []);
