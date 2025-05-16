6d0632a9f707c9b7d6528f5fe505ee5aa986cc4f
Mj Advincula <ehm.advincula13@gmail.com>
2025-05-16 14:18:00 +0800

    generated nextjs project

---
+++ front-end/app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect} from "react";
+import styles from "./page.module.css";
+
+
+export default function Home() {
+  useEffect(() => {
+    const getData = async () => {
+      const query = await fetch("https://jsonplaceholder.typicode.com/users");
+      const response = await query.json();
+      console.log("response hello api", response);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Sorsogon Community Innovation Labs</h1>
+      </main>
+    </div>
+  );
+}
